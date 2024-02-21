ce567b98814c17735294b483426de3a91f46b673
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2024-02-21 10:22:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 
 const port = process.env.PORT || 3010;
+const host = 'https://express-api-ca.onrender.com';
+
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: ${host}`);
   /* eslint-enable no-console */
 });
